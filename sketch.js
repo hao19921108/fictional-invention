3f6843937173080e34fd93ea6b7cb580b2c1a9bc
hao19921108 <howardzhang1108@gmail.com>
2021-03-28 10:09:33 +0800

    new updates

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ if(!input[0]){setInterval( myFunction,2000);
 }
 
 
-var messages=['Corrupted Answers Happens Here', 'Unsearchable Truth','You can type anything. Can you?','No More Hot Search Ranks','Truths got Blurry','Man-made Attention', 'Ask Reddit about this','Unpopular Keywords','Default Answers are PAID', 'Your Input would be purhcased by third parties for Free.',"Taobao wouldn't display an ad because of your inputs.","Facebook is not listening to this.",'Baidu is not listening to this',
-'Is Weibo Real?','Ask Tiktok for a second opinion','How to get featured in Hot Search?'];
+var messages=['Corrupted Answers', "Sexy Photos",'Hao','Unsearchable','996','How to feel like a boss','Has my coworker ask a salary raise yet?','Art to buy at Zhu Jia Jiao','Things to do on this website','Why links below got Blurry','Hao Zhang Shanghai', 'Ask Weibo about Scary','Secrets hidden by Baidu','Are Default Answers PAID?', 'Will someone sell this input field to third party?',"Is Taobao listening to this?","Hao's Art",'What\s meaning of life?','How to rocket Real Estate Market',
+'Is Weibo Real?','Ask Tiktok for a second opinion','How to get featured in Hot Search?','How to buy Hao\'s Website?','God','RADCIAL CHINESE CONTEMPORARY FINE ART','Cold Search','Is Google available now?'];
 
 let i=0;
 let flag=0;
@@ -35,7 +35,13 @@ let linksArray;
 
 
 
-
+function GFG_Fun() { 
+            var img = document.createElement('img'); 
+            img.src =  'logo.png'; 
+        img.width=105;
+           document.getElementsByClassName('logo')[0].appendChild(img); 
+  //          down.innerHTML = "Image Element Added.";  
+        }  
 
 
 
@@ -58,8 +64,11 @@ function addBlackBox(){
         
         document.getElementsByClassName('gn_logo').innerHTML='';
         document.getElementsByClassName('copyright')[0].innerHTML='';
-
+        document.getElementsByClassName('logo')[0].innerHTML='';
         
+         GFG_Fun();
+        document.getElementsByTagName('title')[0].innerHTML='Search Search';
+ document.getElementsByClassName('s-btn-b')[0].innerText='瞎看看';
         flag=1;
         
     }
